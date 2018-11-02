e0ecdbb2f933c14f738adeb064350fb9dc2a9575
ivan4000 <sxln1@mail.ru>
2018-11-02 17:38:13 +0300

    JL107sliders

---
+++ src/com/Main.java
@@ -0,0 +1,32 @@
+package com;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    static JFrame jFrame = getFrame();
+    static JPanel jPanel = new JPanel();
+
+    public static void main(String[] args) {
+        jFrame.add(jPanel);
+        JSlider jSlider=new JSlider(JSlider.HORIZONTAL,0,100,20);
+        jPanel.add(jSlider);
+        jSlider.setMinorTickSpacing(10);
+        jSlider.setMajorTickSpacing(20);
+        jSlider.setPaintTicks(true);
+        jSlider.setSnapToTicks(true);
+        jSlider.setPaintLabels(true);
+    }
+
+    static JFrame getFrame() {
+        JFrame jFrame = new JFrame() {
+        };
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        jFrame.setBounds(dimension.width / 2 - 250, dimension.height / 2 - 150, 500, 500);
+        return jFrame;
+
+    }
+}
